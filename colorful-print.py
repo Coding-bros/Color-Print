5a4d09ca2a0783932c7866a504d355a93d2785ac
Karthik <bkp.karthi@gmail.com>
2020-12-07 17:30:21 +0530

    main.py has been added

---
+++ colorful-print.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+while 1:
+    a = 1
+
+    if a == 1:
+        print('\033[1;31;1mcolorful')
+	a = 2
+	time.sleep(0.1)
+
+    if a == 2:
+        print('\033[1;32;1mcolorful')
+	a = 3
+	time.sleep(0.1)
+
+    if a == 3:
+        print('\033[1;33;1mcolorful')
+	a = 4
+	time.sleep(0.1)
+
+    if a == 4:
+        print('\033[1;34;1mcolorful')
+	a = 5
+	time.sleep(0.1)
+
+    if a == 5:
+        print('\033[1;35;1mcolorful')
+	a = 6
+	time.sleep(0.1)
+
+    if a == 6:
+        print('\033[1;36;1mcolorful')
+	a = 1
+	time.sleep(0.1)
